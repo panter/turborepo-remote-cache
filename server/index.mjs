134e4cb2906af84c4aaf5436983dce1fa0be6721
Marco Wettstein <maw@panter.ch>
2024-07-17 14:32:47 +0200

    try server

---
+++ server/index.mjs
@@ -0,0 +1,21 @@
+import http2 from "node:http2";
+import remix from "@remix-run/express";
+import express from "express";
+import * as build from "../build/index.js";
+const app = express();
+
+// ... code setting up your express app goes here ...
+
+const server = http2.createServer(app);
+
+app.all(
+  "*",
+  remix.createRequestHandler({
+    build,
+  })
+);
+
+const port = process.env.PORT;
+server.listen(port, () => {
+  // ... code to run after your server is running goes here ...
+});
